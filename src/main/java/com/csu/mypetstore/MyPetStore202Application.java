a09baf261ad9fedefe320387bc55de3328895b0e
WONDER <909785560@qq.com>
2024-03-18 06:11:39 +0800

    init mypetstore

---
+++ src/main/java/com/csu/mypetstore/MyPetStore202Application.java
@@ -0,0 +1,13 @@
+package com.csu.mypetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetStore202Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetStore202Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/csu/mypetstore/MyPetStore202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csu.mypetstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetStore202ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
